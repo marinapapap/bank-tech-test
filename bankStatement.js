0c312a58e59437fc2d0c0afb5ebb774ad44e5498
Marina Papapetrou <contact@marinapapap.com>
2023-01-23 11:46:08 +0000

    Passed test 'takes a deposit from the bank class and returns data formatted on a new line after the headings'GREEN.

--- bankStatement.js
+++ bankStatement.js
@@ -5,12 +5,29 @@ class BankStatement {
     this.transactions = [];
   }
 
-  addTransaction(transaction) {
-    
+  addTransaction(record) {
+    this.total += record.transaction()[1];
+    record.transaction().push(this.total);
+    console.log(record.transaction());
+
+    const array = record.transaction().map(x => {
+
+      if(Number.isInteger(x)) {
+        return x.toFixed(2);
+      } else {
+        return x;
+      };
+
+    });
+
+    this.transactions.push(array);
   }
 
   statement() {
-    return "date || credit || debit || balance";
+    this.transactions.push(["date || credit || debit || balance"])
+    return this.transactions.map(x =>
+        x.join(" || ").replace("  ", " ")
+      ).reverse().join("\n");
   }
 }
 
