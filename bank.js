2445efcb0c5c58b0307453a74d7b68e0bfc680e9
Marina Papapetrou <contact@marinapapap.com>
2023-01-24 15:25:32 +0000

    Bank class and tests updated so as to calculate the current balance and add it to each record object.

--- bank.js
+++ bank.js
@@ -2,21 +2,28 @@ class Bank {
 
   constructor() {
     this.records = [];
+    this.currentBalance = 0;
   }
 
   deposit(date, value) {
     const record = {}
+    this.currentBalance += value;
+
     record.date = date.split("/").join("-");
     record.deposit = value;
     record.withdrawal = "";
+    record.balance = this.currentBalance;
     this.records.push(record);
   }
 
   withdrawal(date, value) {
     const record = {}
+    this.currentBalance -= value;
+
     record.date = date.split("/").join("-");
     record.deposit = "";
     record.withdrawal = value;
+    record.balance = this.currentBalance;
     this.records.push(record);
   }
 
